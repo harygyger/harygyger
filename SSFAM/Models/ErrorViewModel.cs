fdf1bb9eca21f87f04402882d435981e386e8760
harrygyger <harry.gyger@gmail.com>
2021-03-21 11:02:32 -0600

    v0.0

---
+++ SSFAM/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SSFAM.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
